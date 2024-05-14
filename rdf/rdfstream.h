35a2cb3d78376e04e74697d4e3f25d0e782fabc7
lbwei <0800680274united@gmail.com>
2024-05-14 19:14:10 +0800

    streaming input

--- rdf/rdfstream.h
+++ rdf/rdfstream.h
@@ -5,15 +5,35 @@
 #include "RdfDedge.h"
 #include "RdfQedge.h"
 
-class rdfstream : public gstream{
+#include <queue>
+#include <climits>
+
+class rdfstream : public gstream
+{
 public:
 	rdfstream(string _dat_set);
 	~rdfstream();
-	
+
 	bool load_edges(int64_t _avg_win_tuple_num);
-	bool is_expire(dEdge* _e_old, dEdge* _e_new);
-	vector<dEdge *> get_batch(ifstream &);
+	void initialize(int64_t window_size);
+	bool is_expire(dEdge *_e_old, dEdge *_e_new);
+	bool nothing_to_send();
+	rdfDedge *next_valid_record();
+	vector<dEdge *> get_batch();
+	vector<dEdge *> next_edges();
 	// int64_t get_timestamp(dEdge *e);
+
+	struct rdfDedgeComparator
+	{
+		bool operator()(rdfDedge *_a, rdfDedge *_b)
+		{
+			return _a->t_sec > _b->t_sec;
+		}
+	};
+
+	priority_queue<rdfDedge *, vector<rdfDedge *>, rdfDedgeComparator> buffer;
+	ifstream *fin;
+	int64_t boundary_time;
 
 private:
 	int64_t avg_span_t;
